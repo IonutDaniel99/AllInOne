0fefbe514fe2aed120c83c8d3f2e54209f950312
ionutdaniel99 <ionut.daniel644@gmail.com>
2022-04-02 21:50:08 +0300

    1.1

--- src/components/LocalizationComponents/LocalizationRowComponent.js
+++ src/components/LocalizationComponents/LocalizationRowComponent.js
@@ -5,8 +5,7 @@ import { getCountry } from '../../utils/getCountry';
 export default function LocalizationRowComponent({gpsData}) {
   const lat = gpsData.coords.latitude;
   const long = gpsData.coords.longitude;
-  const a = getCountry(lat,long)
-  console.log(a)
+  getCountry(lat,long)
   return (
     <View>
       <Text>Manele</Text>
